25a5b3ae9ac26ccc43c570bc4af19e25918a43b1
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-24 15:51:44 +0100

    feat: Add grade and graduate methods in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,6 +12,39 @@ class Person {
 	}
 }
 
+class Student extends Person {
+	constructor(att) {
+		super(att);
+		this.previousBackground = att.previousBackground;
+		this.className = att.className;
+		this.favSubjects = att.favSubjects;
+		this.grade = att.grade;
+	}
+	listsSubjects() {
+		for (let i = 0; i < this.favSubjects.length; i++) {
+			console.log(this.favSubjects[i]);
+		}
+	}
+	PRAssignment(subject) {
+		console.log(`${this.name} has submitted a PR for ${subject}.`);
+	}
+	sprintChallenge(subject) {
+		console.log(`${this.name} has began sprint challenfe on ${subject}.`);
+	}
+	graduate() {
+		if (this.grade > 70) {
+			console.log(`${this.name} graduates!!!!`);
+		} else {
+			ilana.gradeStudent(this);
+			console.log(
+				`${this.name} you have ${
+					this.grade
+				}/100 which is not enought points mate. But I am going to grade your latest test and we will see. Try to check again soon.`
+			);
+		}
+	}
+}
+
 class Instructor extends Person {
 	constructor(att) {
 		super(att);
@@ -25,25 +58,23 @@ class Instructor extends Person {
 	grade(student, subject) {
 		console.log(`${student.name} receives a perfect score on ${subject}.`);
 	}
-}
 
-class Student extends Person {
-	constructor(att) {
-		super(att);
-		this.previousBackground = att.previousBackground;
-		this.className = att.className;
-		this.favSubjects = att.favSubjects;
-	}
-	listsSubjects() {
-		for (let i = 0; i < this.favSubjects.length; i++) {
-			console.log(this.favSubjects[i]);
+	gradeStudent(student) {
+		const randomNumber = function randomizeGrade() {
+			let plusOrMinus = Math.random() < 0.35 ? -1 : 1;
+			let randomPoints = Math.floor(Math.random() * Math.floor(20));
+			return plusOrMinus * randomPoints;
+		};
+		let points = randomNumber();
+		if (student.grade + points > 100) {
+			student.grade = 100;
+		} else if (student.grade + points < 0) {
+			student.grade = 0;
+		} else {
+			student.grade += points;
 		}
-	}
-	PRAssignment(subject) {
-		console.log(`${this.name} has submitted a PR for ${subject}.`);
-	}
-	sprintChallenge(subject) {
-		console.log(`${this.name} has began sprint challenfe on ${subject}.`);
+
+		return `${student.name} : ${student.grade}`;
 	}
 }
 
@@ -71,7 +102,8 @@ const sabrina = new Student({
 	gender: "Female",
 	previousBackground: "Lawyer",
 	className: "WEBEU1",
-	favSubjects: ["React", "Python", "CS"]
+	favSubjects: ["React", "Python", "CS"],
+	grade: 56
 });
 
 const sam = new Student({
@@ -81,7 +113,8 @@ const sam = new Student({
 	gender: "Male",
 	previousBackground: "Doctor",
 	className: "WEBEU3",
-	favSubjects: ["Redux", "Javascript", "CS"]
+	favSubjects: ["Redux", "Javascript", "CS"],
+	grade: 78
 });
 
 const severus = new Student({
@@ -91,7 +124,8 @@ const severus = new Student({
 	gender: "Male",
 	previousBackground: "Accountant",
 	className: "WEBEU5",
-	favSubjects: ["Redux", "Javascript", "CS"]
+	favSubjects: ["Redux", "Javascript", "CS"],
+	grade: 20
 });
 
 //INSTRUCTORS
